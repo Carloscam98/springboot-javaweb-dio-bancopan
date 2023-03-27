04e3ce636256e2d77e2f86591b3480e8b2a0ddb1
CarlosMorato <morato.acarlos@gmail.com>
2023-03-26 23:04:07 -0300

    projeto concluido springboot

---
+++ userdept/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_department(name) VALUES ('Financeiro');
+INSERT INTO tb_department(name) VALUES ('Tecnologia e Operações');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Salluan', 'salluanpereira@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bianca', 'bia_somtri@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Carlos', 'morato.acarlos@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Matheus', 'math.eus@gmail.com');
